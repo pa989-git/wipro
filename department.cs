0022ee9a6d0f9082c22612af13d6063c48c38fa0
pa989-git <patakamurineelima@gmail.com>
2025-01-30 10:32:34 +0530

    Add files via upload

---
+++ department.cs
@@ -0,0 +1,13 @@
+﻿namespace oopsday4
+{
+    internal class Department: Employee // Inheritance
+    {
+        public string deptName { get; set; }
+        public decimal salary { get; set; }
+        public override void Display()
+        {
+            Console.WriteLine("Department salary: " + deptsalary);
+            Console.WriteLine("Department Name: " + deptName);
+        }
+    }
+}
