7966ea7373648fb392c71bfe1d05270f729b159f
pa989-git <patakamurineelima@gmail.com>
2025-01-30 10:58:17 +0530

    day4

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace oopsday4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.Empid = 11;
+            emp.EmpName = "Test";
+
+            //  emp.Display();
+
+            Department dpt = new Department(); //creating instance for the derived class
+            dpt.Empid = 111;
+            dpt.EmpName = "Priya";
+            dpt.DeptName = "Develoepr";
+            dpt.Salary = 50000;
+
+            //dpt.Display();
+            //  dpt.Show();
+
+            Manager mgr = new Manager();
+            mgr.Empid = 111;
+            mgr.EmpName = "Priya";
+            mgr.DeptName = "Develoepr";
+            mgr.Salary = 50000;
+            mgr.ManagerName = "Riyaz";
+
+            mgr.Display();
+            //  mgr.show();
+            //  mgr.detail();
+
+
+        }
+    }
+}
---
+++ manager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiproOOPSconcepts
+{
+    internal class Manager : Department
+    {
+        public string ManagerName { get; set; }
+
+        public override void Display()
+        {
+            base.Display(); //call display method of department class
+            Console.WriteLine("Manager Name" + ManagerName);
+        }
+    }
+}
