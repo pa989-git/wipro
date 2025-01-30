22c865ed16c52e36e102f25940a9438a6ff33db0
pa989-git <patakamurineelima@gmail.com>
2025-01-30 10:24:10 +0530

    Add files via upload
    employee.cs

---
+++ employee.cs
@@ -0,0 +1,16 @@
+﻿name space oopsday4
+{
+    internal class Employee
+{
+    public int Empid { get; set };
+    public string Empname { get; set; }
+    public virtual void Display()
+    {
+        Console.WriteLine("Employee ID: " + Empid);
+        Console.WriteLine("Employee Name: " + Empname);
+    }
+    {
+    }
+}
+
+}
