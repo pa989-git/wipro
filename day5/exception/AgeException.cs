1761fc42c5f48635f49c3226db6cae7bf01d3599
pa989-git <patakamurineelima@gmail.com>
2025-01-31 12:29:24 +0530

    Add files via upload

---
+++ day5/exception/AgeException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exceptionhandling
+{
+    internal class AgeException :Exception 
+    {
+        public AgeException(string errmsg) : base(errmsg)
+        { }
+    }
+}
+
