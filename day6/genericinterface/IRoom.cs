bcae1df9fb10af9ed2b444238f3fd9a665a76552
pa989-git <patakamurineelima@gmail.com>
2025-02-01 11:17:09 +0530

    Add files via upload

---
+++ day6/genericinterface/IRoom.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericINterface
+{//Generic INterface - Generic Type <T>
+    internal interface IRoom<T> //T is  a class 
+    {
+        IEnumerable<T> GetAll();
+        void AddRoom(T item);
+    }
+}
