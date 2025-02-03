46e537009d7b6023f5daa4002b391ae955a282ee
neelu <patakamurineelima@gmail.com>
2025-02-03 15:55:56 +0530

    Add files via upload

---
+++ day7/genericDelegate/Program.cs
@@ -0,0 +1,22 @@
+﻿using GenericDelegate;
+
+class Program
+{
+    delegate TOutput CovariantContravariantDelegate<in TInput, out TOutput>(TInput input);
+
+    static Vehicle ProcessVehicle(Vehicle v)
+    {
+        Console.WriteLine("Processing: " + v.GetType().Name);
+        return new Car(); 
+    }
+
+    static void Main()
+    {
+        CovariantContravariantDelegate<Car, Vehicle> handler = ProcessVehicle;
+
+        Car myCar = new Car();
+        Vehicle result = handler(myCar); // Passing Car (contravariance), getting Vehicle (covariance)
+
+        Console.WriteLine("Returned: " + result.GetType().Name);
+    }
+}
